29631c3fa45e5f603a8d5acf82fdce6a8dac3781
GuyfH3CTOR <108801997+GuyfH3CTOR@users.noreply.github.com>
2024-03-19 19:02:57 +0100

    +

--- HacknSlash/Assets/8_Scripts/Particles/PlayEffect.cs
+++ HacknSlash/Assets/8_Scripts/Particles/PlayEffect.cs
@@ -9,18 +9,11 @@ public class PlayEffect : MonoBehaviour
     [Header("========== Particles ==========")]
     [Header("References")]
     public AudioSource effectSound;
-    public GameObject[] g_Particles;
     public List<ParticleSystem> particleSystems = new List<ParticleSystem>();
 
     [Header("Debuging")]
     public bool play;
 
-    void Start(){
-        // Get particules of parents and Children components
-        for(int i = 0; i < g_Particles.Length; i++){
-            particleSystems.Add(g_Particles[i].GetComponent<ParticleSystem>());
-        }
-    }
     void Update(){
         // Debuging
         if(play){
@@ -31,9 +24,9 @@ public class PlayEffect : MonoBehaviour
 
     public void Play(){
         // Play all particules in particleSystems with additional Effects
-        foreach(ParticleSystem particuls in particleSystems){
-            particuls.Play();
-            effectSound.Play();
+        for(int i = 0; i < particleSystems.Count; i++){
+            particleSystems[i].Play();
         }
+        effectSound.Play();
     }
 }
--- HacknSlash/Assets/Resources/Spell/Script/Charging Bomb.cs
+++ HacknSlash/Assets/Resources/Spell/Script/Charging Bomb.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,39 +9,67 @@ public class ChargingBomb : MonoBehaviour
     [Header("========== Charging Bomb ==========")]
     
     [Header("#### Settings ####")]
-    public float CountUntilExplosion = 30;
-    public float ExplosionDamage = 20;
-
+    public float ExplosionDamage = 5;
     [Space]
-    
+    [Header("#### Countdowns ####")]
+    // public
+    public float CountUntilHit = 5;
+    public float HitEffectDelayToHit = 0;
+    public float destroyGameObjectDelay;
+    // private
+    private float InUseEffectCountdown;
+    [Space]
     [Header("#### References ####")]
+    public GameObject zoneEffect;
     public Slider slider;
-    public GameObject g_entitiesDetector;
-    private EntitiesDetector entitiesDetector;
+    public EntitiesDetector entitiesDetector;
+    public PlayEffect playEffectCharge;
+    public PlayEffect playEffectExplosion;
 
     void Start(){
-        slider.maxValue = CountUntilExplosion;
-        entitiesDetector = g_entitiesDetector.GetComponent<EntitiesDetector>();
+        // Set In Use Effect
+        slider.maxValue = CountUntilHit;
+        InUseEffectCountdown = CountUntilHit;
+        CountUntilHit = CountUntilHit - HitEffectDelayToHit;
+        // Wait for explosion
+        StartCoroutine(InUseDelay());
     }
 
     void Update(){
-        CountDownClock();
+        InUseUpdate();
     }
 
-    void CountDownClock(){
-        CountUntilExplosion -= Time.deltaTime;
-        slider.value = CountUntilExplosion;
-        if(CountUntilExplosion <= 0)
-        {
-            for(int i = 0; i < entitiesDetector.EntitiesDetected.Count; i++){
+    void InUseUpdate(){
+        // Update In Use Effect
+        InUseEffectCountdown -= Time.deltaTime;
+        slider.value = InUseEffectCountdown;
+    }
+
+    IEnumerator InUseDelay(){
+        // Debug.Log("CountUntilHit : "+CountUntilHit);
+        // Debug.Log("HitEffectDelay : "+HitEffectDelayToHit);
+        yield return new WaitForSeconds(CountUntilHit);
+        // play Hit Effect
+        playEffectExplosion.Play();
+        StartCoroutine(HitDelay());
+    }
+
+    IEnumerator HitDelay(){
+        // zoneEffect.SetActive(false);
+        yield return new WaitForSeconds(HitEffectDelayToHit);
+        // Hide In Use Effect
+        zoneEffect.SetActive(false);
+        // Damage Entity_Damagable
+        for(int i = 0; i < entitiesDetector.EntitiesDetected.Count; i++){
             if(entitiesDetector.EntitiesDetected[i].GetComponent<Entity_Damagable>() != null){
                 entitiesDetector.EntitiesDetected[i].GetComponent<Entity_Damagable>().UpdateLife(-ExplosionDamage);
-                // if(entitiesDetector.EntitiesDetected[i].GetComponent<Entity_Damagable>().UpdateLife(-ExplosionDamage)){
-                //     entitiesDetector.EntitiesDetected.RemoveAt(i);
-                // }
             }
         }
-            Destroy(gameObject);
-        }
+        StartCoroutine(DestroyDelay());
+    }
+
+    IEnumerator DestroyDelay(){
+        yield return new WaitForSeconds(destroyGameObjectDelay);
+        Destroy(gameObject);
     }
 }
